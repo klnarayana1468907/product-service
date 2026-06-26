968ae9d7e1510147b9d6e9eaff83cf2be731fa43
Lakshmi Narayana <klnarayana.1468907@gmail.com>
2026-06-26 12:41:12 +0530

    Integrated Eureka Client, Service Discovery and Load Balancing

--- src/main/java/com/company/ecommerce/product_service/kafka/ProductKafkaConsumer.java
+++ src/main/java/com/company/ecommerce/product_service/kafka/ProductKafkaConsumer.java
@@ -7,33 +7,22 @@ import com.company.ecommerce.common.event.RestoreInventoryEvent;
 import com.company.ecommerce.product_service.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProductKafkaConsumer {
-	
-    private final ProductService productService;
-    
-    @KafkaListener(
-            topics = "inventory-restore",
-            groupId = "product-group"
-    )
-    public void restoreInventory(RestoreInventoryEvent event) {
 
-        System.out.println(
-                "Restoring inventory for product: "
-                + event.getProductId()
-        );
+	private final ProductService productService;
 
-        productService.restoreStock(
-                event.getProductId(),
-                event.getQuantity()
-        );
+	@KafkaListener(topics = "inventory-restore", groupId = "product-group")
+	public void restoreInventory(RestoreInventoryEvent event) {
 
-        System.out.println(
-                "Inventory restored successfully"
-        );
-    }
+		log.info("Restoring inventory for product: {}" + event.getProductId());
 
+		productService.restoreStock(event.getProductId(), event.getQuantity());
+
+		log.info("Inventory restored successfully");
+	}
 
 }
